f1ded9b99039f06cbf68bcb7ab1562f407c22833
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2019-08-25 16:07:44 +0200

    Handle the visibility of long and short url texts in case the input text area is full and empty

--- src/App.svelte
+++ src/App.svelte
@@ -2,61 +2,68 @@
 	import Navigation from './Navigation.svelte';
 	let name = '';
 	let convertedURL = '';
+	let ableToConvert = false;
 	function tinyURL(name) {
 		console.log(name);
 		convertedURL = name + 'testing';
 	}
 	function copyToClipboard() {
-		// var copyText = document.getElementById('tinyURL');
-		// console.write(copyText)
-		// // copyText.select();
-		// copyText.setSelectionRange(0, 99999);
-		// // copyText.select()
-
-		// document.execCommand('copy');
-		// alert('Copied the text: ' + copyText.value);
 		var range = document.createRange();
-                    range.selectNode(document.getElementById("tinyURL"));
-                    window.getSelection().removeAllRanges(); // clear current selection
-                    window.getSelection().addRange(range); // to select text
-                    document.execCommand("copy");
-                    window.getSelection().removeAllRanges();// to deselect
+		range.selectNode(document.getElementById('tinyURL'));
+		window.getSelection().removeAllRanges(); // clear current selection
+		window.getSelection().addRange(range); // to select text
+		document.execCommand('copy');
+		window.getSelection().removeAllRanges(); // to deselect
+	}
+	$: if (name == '') {
+		convertedURL = '';
 	}
 </script>
 
 <div>
 	<Navigation />
 </div>
-<div class="mt-3">
-	<h1>Long URL converter</h1>
-</div>
-<div class="container mt-5">
+
+<div class="container ">
+	<div class="mt-3">
+		<h1>Long URL converter</h1>
+	</div>
 	<div class="row">
-		<!--[Tasks-Data]-->
 		<div class="col-sm-12">
 
-			<input bind:value={name} />
-
-			<!--[Convert-BTN]-->
-			<button on:click={() => tinyURL(name)}>Convert</button>
-			{#if convertedURL != ''}
-				<!--[Long-url]-->
+			<!--[Long-URL-Input]-->
+			<div class="d-flex justify-content-start">
 				<div>
-					<b>Long URL:</b>
-					<p>{name}</p>
+					<input bind:value={name} />
 				</div>
-				<!--[Short-Url]-->
-				<div class="d-flex justify-content-start">
-					<!--[Short-Url-Text]-->
+
+				<!--[Convert-BTN]-->
+				<div>
+					<button class="btn btn-primary ml-2" on:click={() => tinyURL(name)}>
+						Convert
+					</button>
+				</div>
+			</div>
+			{#if convertedURL != '' && name != ''}
+				<div class="mt-4">
+					<!--[Long-url]-->
 					<div>
-						<b>Short URL:</b>
-						<p id="tinyURL">{convertedURL}</p>
+						<b>Long URL:</b>
+						<p>{name}</p>
 					</div>
-					<!--[Copy-BTN]-->
-					<div class="ml-3">
-						<button class="btn btn-light" on:click={() => copyToClipboard()}>
-							Copy
-						</button>
+					<!--[Short-Url]-->
+					<div class="d-flex justify-content-start">
+						<!--[Short-Url-Text]-->
+						<div>
+							<b>Short URL:</b>
+							<p id="tinyURL">{convertedURL}</p>
+						</div>
+						<!--[Copy-BTN]-->
+						<div class="ml-3">
+							<button class="btn btn-light" on:click={() => copyToClipboard()}>
+								Copy
+							</button>
+						</div>
 					</div>
 				</div>
 			{/if}
