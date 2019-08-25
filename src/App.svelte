a7f19c5bf8a5ba3e3ee189b9f215cc79253ee834
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2019-08-25 13:50:21 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+	let name = '';
+	let convertedURL = '';
+	function tinyURL(name) {
+		console.log(name);
+		convertedURL = name + 'testing';
+	}
+</script>
+
+<div>
+	<h1>Long URL converter</h1>
+</div>
+<div class="container mt-5">
+	<div class="row">
+		<!--[Tasks-Data]-->
+		<div class="col-sm-12">
+
+			<input bind:value={name} />
+
+			<!--[Convert-BTN]-->
+			<button on:click={() => tinyURL(name)}>Convert</button>
+			{#if convertedURL != ''}
+				<!--[Long-url]-->
+				<div>
+					<b>Long URL:</b> <p>{name}</p>
+				</div>
+				<!--[Short-Url]-->
+				<div>
+					<b>Short URL:</b> <p>{convertedURL}</p>
+				</div>
+			{/if}
+
+		</div>
+	</div>
+</div>
